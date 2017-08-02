50bd637c058e0547daf723a8c8aa51e1811219b4
노용훈 <mogi3206@noyonghunui-MacBook-Pro.local>
2017-08-02 15:54:38 +0900

    first commit

---
+++ Today diary/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Today diary
+//
+//  Created by 노용훈 on 2017. 8. 2..
+//  Copyright © 2017년 yonghun.noh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textInput: UITextField!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var contentLabel: UILabel!
+   
+    @IBAction func buttonPressed(_ sender: Any) {
+        let text = textInput.text
+        textLabel.text = text
+        
+        let content = textArea.text
+        contentLabel.text = content
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
